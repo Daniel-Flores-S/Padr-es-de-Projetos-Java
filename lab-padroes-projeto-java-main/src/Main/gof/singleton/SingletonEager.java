8abd7c64e12f67855abfb79e6136fb3863843ff9
Daniel <danielfloresda802@gmail.com>
2022-03-07 12:40:23 -0300

    first commit

---
+++ lab-padroes-projeto-java-main/src/Main/gof/singleton/SingletonEager.java
@@ -0,0 +1,19 @@
+package Main.gof.singleton;
+
+/**
+ * Singleton "apressado".
+ * 
+ * @author falvojr
+ */
+public class SingletonEager {
+
+	private static SingletonEager instancia = new SingletonEager();
+	
+	private SingletonEager() {
+		super();
+	}
+	
+	public static SingletonEager getInstancia() {
+		return instancia;
+	}
+}
---
+++ lab-padroes-projeto-java-main/src/Main/gof/strategy/Comportamento.java
@@ -0,0 +1,5 @@
+package Main.gof.strategy;
+
+public interface Comportamento {
+	void mover();
+}
